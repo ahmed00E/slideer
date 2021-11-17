7c3afea0ebdf5e841eec103b778186ecbdb01275
ahmed00E <84589474+ahmed00E@users.noreply.github.com>
2021-11-17 21:34:55 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Slider from './Components/Slider/Slider'
+import {Header} from  './Components/Header';
+ 
+ 
+ 
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+      <Slider />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h1>
+          Best Movies  
+        </h1>
+    )
+}
